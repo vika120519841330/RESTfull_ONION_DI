43ef1cd26c6018b8e5bcae8926486b892236bd0c
Vika Strigo <vika120519841330@gmail.com>
2019-08-05 00:54:21 +0300

    adding rep

---
+++ ClientAccount_REST_Onion_DI_v2/DependencyInjection/Modules/InfrastructureModule.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Practices.Unity;
+using Infrastructure.Interfaces;
+using InfrastructureServices.Repositories;
+
+namespace DependencyInjection.Modules
+{
+    public class InfrastructureModule : IModule
+    {
+        public void Register(IUnityContainer container)
+        {
+            container.RegisterType<IClientRepository, FakeRepository>(new HierarchicalLifetimeManager());
+            container.RegisterType<IAccountRepository, FakeRepository>(new HierarchicalLifetimeManager());
+        }
+    }
+}
